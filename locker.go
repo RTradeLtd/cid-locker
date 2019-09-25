b576258c924017fcaad147f6698b2a1e160249f9
postables <postables@rtradetechnologies.com>
2019-09-24 17:02:11 -0700

    locker.go: rename comments

--- locker.go
+++ locker.go
@@ -37,7 +37,7 @@ func (cl *CIDLocker) Create(cid gocid.Cid) {
 	}
 }
 
-// Exists check if we have a lock for this peerID
+// Exists check if we have a lock for this cid
 func (cl *CIDLocker) Exists(cid gocid.Cid) bool {
 	cl.mux.RLock()
 	_, exists := cl.locks[cid]
@@ -45,7 +45,7 @@ func (cl *CIDLocker) Exists(cid gocid.Cid) bool {
 	return exists
 }
 
-// Lock obtains a lock for the peerID
+// Lock obtains a lock for the cid
 func (cl *CIDLocker) Lock(cid gocid.Cid) {
 	cl.Create(cid)
 	cl.mux.RLock()
@@ -53,7 +53,7 @@ func (cl *CIDLocker) Lock(cid gocid.Cid) {
 	cl.mux.RUnlock()
 }
 
-// Unlock reverts the peerID lock
+// Unlock reverts the cid lock
 func (cl *CIDLocker) Unlock(cid gocid.Cid) {
 	cl.Create(cid)
 	cl.mux.RLock()
